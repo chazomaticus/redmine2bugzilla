dcb6c34c92f61336e420e32a4a49ac0098fea978
Charles Lindsay <chaz@yorba.org>
2013-11-01 16:31:44 -0700

    Add more data, usage tweaks

--- redmine2bugzilla.py
+++ redmine2bugzilla.py
@@ -2,13 +2,11 @@
 #
 # redmine2bugzilla - export Redmine bugs to Bugzilla-importable XML
 
-import sys
-import time
-import re
-import urllib2
-import argparse
+import sys, time, re
+import urllib2, base64
 from BeautifulSoup import BeautifulSoup
 from html2text import html2text
+import argparse
 
 redmine_base = 'http://redmine.example.com'
 
@@ -50,45 +48,51 @@ def scrape(bug_id):
     assignee = attributes('td', 'assigned-to')[0]
     attachments_divs = issue('div', 'attachments')
     attachments_div = attachments_divs[0] if attachments_divs else None
+    histories = html('div', id='history')
 
     data = {}
+    data['id'] = bug_id
+    data['url'] = url
+    data['project'] = to_s(html.h1, True)
     data['title'] = to_s(issue('div', 'subject')[0].h3)
     data['author'] = to_s(author.a)
     data['assignee'] = to_s(assignee.a if assignee.a != None else assignee)
     data['created'] = time.strptime(times[0]['title'], timestamp_format)
-    data['updated'] = time.strptime(times[1]['title'], timestamp_format)
+    data['updated'] = time.strptime(times[1]['title'], timestamp_format) if len(times) > 1 else None
     data['status'] = to_s(attributes('td', 'status')[0], True)
     data['priority'] = to_s(attributes('td', 'priority')[0], True)
     data['category'] = to_s(attributes('td', 'category')[0], True)
     data['version'] = to_s(attributes('td', 'fixed-version')[0], True)
     data['description'] = to_text(issue('div', 'wiki')[0])
-    data['history'] = to_text(html('div', id='history')[0])
+    data['history'] = to_text(histories[0]) if histories else None
 
     attachments = []
     for p in attachments_div('p') if attachments_div != None else []:
         description = to_s(p.a.nextSibling)
         author_match = attachment_author_re.search(to_s(p('span', 'author')[0]))
+        attachment_url = "{0}{1}".format(redmine_base, attachment_url_re.sub(attachment_url_sub, p.a['href']))
 
         attachment = {}
+        attachment['url'] = attachment_url
         attachment['filename'] = to_s(p.a)
         attachment['description'] = description.lstrip(' -').strip() if description != None else None
         attachment['author'] = author_match.group(1)
         attachment['created'] = time.strptime(author_match.group(2), timestamp_format)
-        attachment['url'] = "{0}{1}".format(redmine_base, attachment_url_re.sub(attachment_url_sub, p.a['href']))
+        attachment['data'] = base64.b64encode(urllib2.urlopen(attachment_url).read())
         attachments.append(attachment)
 
     data['attachments'] = attachments
     return data
 
 def print_data(data, pre=''):
-    """Prints the results of scrape() in a relatively sane format"""
+    """Prints the results of scrape() in a debug format"""
 
     for item in sorted(data.keys()):
         if type(data[item]) is list: # Assume list of dicts
             for elem in data[item]:
                 print_data(elem, "{0}[]/".format(item))
         else:
-            print("{0}{1:<12}: {2}".format(pre, item, data[item]))
+            print("{0}{1:<12}: {2}".format(pre, item, data[item] if item != 'data' else "{0}...".format(data[item][:48])))
 
 def main(argv=None):
     if argv is None:
@@ -99,19 +103,20 @@ def main(argv=None):
     parser = argparse.ArgumentParser(prog=argv[0],
             description="export Redmine bugs to Bugzilla-importable XML")
     parser.add_argument('-r', '--redmine-base',
-            help="redmine base URL, default: {0}".format(redmine_base))
-    parser.add_argument('-s', '--scrape', metavar='BUG_ID', action='append', default=[],
-            help="scrape and print data from the bug id")
+            help="Redmine base URL, default: {0}".format(redmine_base))
+    parser.add_argument('-s', '--scrape', metavar='BUG_ID', action='append',
+            help="don't export; scrape and print data from the bug ids")
     args = parser.parse_args(argv[1:])
 
     if args.redmine_base != None:
         redmine_base = args.redmine_base
-
-    for bug in args.scrape:
-        print("Bug {0}".format(bug))
-        print("----")
-        print_data(scrape(bug))
-        print("")
+    if args.scrape != None:
+        for bug in args.scrape:
+            print("Bug {0}".format(bug))
+            print("----")
+            print_data(scrape(bug))
+            print("")
+        return 0
 
     return 0
 
